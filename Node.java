4617a0c4592c4ae7e4a57068613c40987bcd8ec3
swathisuja <swathivpm@gmail.com>
2017-05-28 20:20:15 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+package com.binary.tree;
+
+/*This class defines the structure of the node*/
+public class Node {
+
+	Node left, right;
+	int data;
+
+	Node(int key) {
+		data = key;
+		left = null;
+		right = null;
+
+	}
+}
